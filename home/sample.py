18d96c48fbd3a8098e6a361aedf8925ef68d0c59
chololong <jeremykandt2@gmail.com>
2021-04-17 18:14:08 +0800

    updated Creds

--- home/sample.py
+++ home/sample.py
@@ -11,9 +11,10 @@ from newspaper import Article
 import datetime as date
 import os
 import hashlib
-class Sample():
-    def loadSample(self,url):
-        print("Here 1")
+
+class Credibility():
+    def loadCredibility(self,url):
+        print("Here 1:load everything")
         #embed = tf.saved_model.load('home\saved_model.pb')
         #os.environ["TFHUB_MODEL_LOAD_FORMAT"] = "UNCOMPRESSED"
         os.environ["TFHUB_CACHE_DIR"] = 'D:/Dev/News Aggregator/tmp/tfhub'
@@ -24,11 +25,16 @@ class Sample():
         misleading = 'home\model_misleading.h5'
         opinion = 'home\model_opinion.h5'
         sarcasm = 'home\model_sarcasm.h5'
-        print("Here 2")
+
+
+        print("Here 2:parse article and build model")
         article = Article(url)
         article.download() 
         article.parse()
         news_title= article.title
+        news_image = article.top_image
+
+
         model = Sequential()
         model.add(LSTM(32))
         model.add(Dense(2, activation='softmax'))
@@ -38,10 +44,32 @@ class Sample():
 
         model.built = True
 
-        print("Here 3")
+        print("Here 3: load opinion model")
+        opinion_prediction = self.opinionScore(self,opinion,url,embed)
+        #print('Predicted class: ', prediction)
+        #print('Real class:  ', sample_Y)
+
+
+
+        print("Here 4:load sarcasm model")
+        sarcasm_prediction = self.sarcasmScore(self,sarcasm,embed,news_title)
+        #print('Predicted class: ', prediction)
+       # print('Real class:  ', sample_Y)
+
+
+
+        print("Here 5:load misleading model")
+        misleading_prediction = self.misleadingScore(self,misleading,embed,news_title)
+        #print('Predicted class: ', misleading_prediction)
+        #print('Real class:  ', sample_Y)
+        print("Here 6: get relevance score")
+        rel_score = self.relevanceScore(article)
+        
+        return misleading_prediction, opinion_prediction, sarcasm_prediction, rel_score, news_title, news_image
+
+    def opinionScore(self,opinion,url,embed):
         new_model=load_model(opinion)
         new_model.summary()
-        print("hi")
 
         article_list = {'corpus': [url],
                 'label': [1]
@@ -52,15 +80,12 @@ class Sample():
         max_length = self.get_max_length_url(self,article_sample)
         sample_X, sample_Y = self.preprocess_url(self,article_sample,embed,max_length)
         opinon_prediction = new_model.predict(sample_X)[0][1]
-        #print('Predicted class: ', prediction)
-        #print('Real class:  ', sample_Y)
 
+        return opinon_prediction*100
 
-
-        print("Here 4")
+    def sarcasmScore(self,sarcasm,embed,news_title):
         new_model=load_model(sarcasm)
         new_model.summary()
-        print("hi")
 
         article_list = {'corpus': [news_title],
                 'label': [1]
@@ -68,31 +93,27 @@ class Sample():
         article_sample = pd.DataFrame(article_list, columns = ['corpus', 'label'])
 
         article_sample= article_sample[article_sample['corpus'].notnull()]
-        max_length = self.get_max_length(self,article_sample)
-        sample_X, sample_Y = self.preprocess(self,article_sample,embed,max_length)
+        max_length = self.get_max_length_url(self,article_sample)
+        sample_X, sample_Y = self.preprocess_url(self,article_sample,embed,max_length)
         sarcasm_prediction = new_model.predict(sample_X)[0][1]
-        #print('Predicted class: ', prediction)
-       # print('Real class:  ', sample_Y)
 
+        return sarcasm_prediction*100
 
-
-        print("Here 5")
+    def misleadingScore(self,misleading,embed,news_title):
         new_model=load_model(misleading)
         new_model.summary()
-        print("hi")
-
         article_list = {'corpus': [news_title],
                 'label': [1]
                 }
         article_sample = pd.DataFrame(article_list, columns = ['corpus', 'label'])
-
         article_sample= article_sample[article_sample['corpus'].notnull()]
         max_length = self.get_max_length(self,article_sample)
         sample_X, sample_Y = self.preprocess(self,article_sample,embed,max_length)
         misleading_prediction = new_model.predict(sample_X)[0][1]
-        #print('Predicted class: ', misleading_prediction)
-        #print('Real class:  ', sample_Y)
-        print("Here 6")
+
+        return misleading_prediction*100
+
+    def relevanceScore(article):
         today = date.datetime.now()
         newsDate = article.publish_date
 
@@ -120,10 +141,10 @@ class Sample():
             multiplier = multiplier + 0.25
 
         rel_score = score - (d * multiplier)
-        if score < 0:
-            score = 0
-        
-        return misleading_prediction, opinon_prediction, sarcasm_prediction, rel_score
+        if rel_score < 0:
+            rel_score = 0
+
+        return rel_score
 
     def get_max_length_url(self,df):
         """
@@ -248,45 +269,6 @@ class Sample():
         X = np.array(padded_encoded_reviews)
         Y = np.array(encoded_labels)
         return X, Y
-    def sampletest(self,urlTest):
-        misleading, opinion,sarcasm,rel_score=self.loadSample(self,urlTest)
-        return misleading, opinion, sarcasm, rel_score
-
-class relevance():
-    from newspaper import Article
-    
-
-    url = 'https://cnnphilippines.com/news/2021/4/10/new-community-quarantine-classification-ncr-plus.html'
-    article = Article(url)
-    #use this to access other functions of the article
-    article.download() 
-    article.parse()
-
-    #get today's datetime and also the article's published datetime
-    today = date.datetime.now()
-    newsDate = article.publish_date
-
-    rel = today - newsDate 
-    days = rel.days 
-    print(days, "day(s)")
-    def relevanceScore(d):
-        score = 100
-        multiplier = 1
-
-        if d//7 >= 4:
-            multiplier = 2
-        elif d > 7:
-            multiplier = multiplier + ((d // 7) * 0.5)
-        elif d >= 5:
-            multiplier = multiplier + 0.35
-        elif d >= 3:
-            multiplier = multiplier + 0.3
-        elif d >= 2:
-            multiplier = multiplier + 0.25
-
-        score = score - (d * multiplier)
-        if score < 0:
-            score = 0
-        return score
-
-    print(relevanceScore(days))
+    def credtest(self,urlTest):
+        misleading, opinion,sarcasm,rel_score,news_title,news_image=self.loadCredibility(self,urlTest)
+        return misleading, opinion, sarcasm, rel_score,news_title,news_image
