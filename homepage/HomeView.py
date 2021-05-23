24eb151085b198b2a5a5864c70f404a86af8e141
peterson1999 <51360811+peterson1999@users.noreply.github.com>
2021-05-24 00:18:47 +0800

    dsadasd
    dsasadasd

---
+++ homepage/HomeView.py
@@ -0,0 +1,94 @@
+from django.conf.urls import url
+from django.http.response import HttpResponse
+from django.shortcuts import render, redirect
+from django.views.generic import View
+
+import requests
+from article.Article import Article
+import datetime
+
+# Create your views here.
+
+
+class HomeIndexView(View):
+    def displayArticles( request):
+        context=HomeIndexView.showLatestUpdates()
+        return render(request, 'home.html', context)
+
+    def getTopArticles():
+        n=Article.getArticleAll()  
+        return n
+
+    def showLatestUpdates():
+        flag = False
+        url = 'http://api.openweathermap.org/data/2.5/forecast?q={}&units=metric&appid=97c08871353e0aee15a30d25127bcd1f'
+        city = 'Cebu City'
+
+        if not flag:
+            r = requests.get(url.format(city)).json()
+
+        city_weather = {
+            'city': city,
+            'temperature': r['list'][0]['main']['temp'],
+            'description': r['list'][0]['weather'][0]['description'],
+            'icon': r['list'][0]['weather'][0]['icon'],
+            
+            'dt1': datetime.datetime.strptime(str(r['list'][1]['dt_txt'][5:7]), "%m").strftime("%b") + " " + r['list'][1]['dt_txt'][8:10],
+            'temperature1': r['list'][1]['main']['temp'],
+            'description1': r['list'][1]['weather'][0]['description'],
+            'icon1': r['list'][1]['weather'][0]['icon'],
+
+            'dt2': datetime.datetime.strptime(str(r['list'][6]['dt_txt'][5:7]), "%m").strftime("%b") + " " + r['list'][6]['dt_txt'][8:10],
+            'temperature2': r['list'][2]['main']['temp'],
+            'description2': r['list'][2]['weather'][0]['description'],
+            'icon2': r['list'][2]['weather'][0]['icon'],
+
+            'dt3': datetime.datetime.strptime(str(r['list'][13]['dt_txt'][5:7]), "%m").strftime("%b") + " " + r['list'][13]['dt_txt'][8:10],
+            'temperature3': r['list'][3]['main']['temp'],
+            'description3': r['list'][3]['weather'][0]['description'],
+            'icon3': r['list'][3]['weather'][0]['icon'],
+
+            'dt4': datetime.datetime.strptime(str(r['list'][21]['dt_txt'][5:7]), "%m").strftime("%b") + " " + r['list'][21]['dt_txt'][8:10],
+            'temperature4': r['list'][4]['main']['temp'],
+            'description4': r['list'][4]['weather'][0]['description'],
+            'icon4': r['list'][4]['weather'][0]['icon'],
+
+        }
+
+        url2 = 'https://api.covid19api.com/live/country/philippines/status/confirmed/date/2020-05-01T13:13:30Z'
+        if not flag:
+            r2 = requests.get(url2.format()).json()
+
+        icon = ""
+        if r2[-1]['Confirmed'] >= r2[-2]['Confirmed']:
+            icon = "bi bi-arrow-up-right"
+        else:
+            icon = "bi bi-arrow-down-left"
+
+        phil_covid = {
+            'infected':  format(r2[-1]['Confirmed'], ',d'),
+            'icon': icon,
+            'active': format(r2[-1]['Active'], ',d'),
+            'recovered': format(r2[-1]['Recovered'], ',d'),
+            'deceased': format(r2[-1]['Deaths'], ',d'),
+        }
+        
+        top_articles= HomeIndexView.getTopArticles()
+        print(top_articles)
+        context = {'city_weather': city_weather,'phil_covid': phil_covid,'top_articles':top_articles}
+        flag = True
+        return context
+    
+#    def displayArticles(request):
+#        if "check" in request.POST:
+#            form = URLForm(request.POST)
+#            if form.is_valid():
+#                urlTest = request.POST.get("url")
+#                global val
+#                def val():
+#                    return urlTest
+#                return redirect('article:article')
+#            else:
+#                print("home")
+#                print(form.errors)
+#                return HttpResponse("Not Valid!")
