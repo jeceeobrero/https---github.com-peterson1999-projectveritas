e87249cefb34a21c5e01b6d9d669266e180cfa6d
jeceeobrero <jeceeobrero@gmail.com>
2021-09-27 22:21:23 +0800

    added newsoutlet to url

--- newsoutlet/urls.py
+++ newsoutlet/urls.py
@@ -4,5 +4,5 @@ from newsoutlet.OutletView import OutletIndexView as view
 app_name = "newsoutlet"
 
 urlpatterns = [
-    path('', view.displayOutlets, name="newsoutlet"),
+    path('newsoutlet', view.displayOutlets, name="newsoutlet"),
 ]
